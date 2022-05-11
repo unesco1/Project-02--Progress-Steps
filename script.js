ba5ab0b56ebba42864816231cfc97626f3513c4f
Unesco <younes.tahiri6@gmail.com>
2022-05-11 23:12:09 +0100

    Projet 02 modified

--- script.js
+++ script.js
@@ -4,31 +4,43 @@ let prevBtn = document.querySelector('button:first-of-type');
 let nextBtn = document.querySelector('button:nth-of-type(2)');
 let i = 0;
 
+
+
 function next() {  
+
     myBorder[i].classList.add('animated');
     i++;
     myCercle[i].classList.add('border-act');
-
-    if (i === 0) {
+    
+    if (i <= 0) {
         prevBtn.setAttribute('disabled', "disabled");
         nextBtn.removeAttribute('disabled');
-    } else if (i > 0 && i === 3) {
+    } else if (i > 0 && i < 3) {
+        prevBtn.removeAttribute('disabled');
+        nextBtn.removeAttribute('disabled');
+    } else {
         prevBtn.removeAttribute('disabled');
         nextBtn.setAttribute('disabled', "disabled");
     };
+    console.log(i);
 };
 
+
 function prev() {
+
     myCercle[i].classList.remove('border-act');
     i--;
     myBorder[i].classList.remove('animated');
-    if (i === 0) {    
-        prevBtn.setAttribute('disabled', "disabled");
-        nextBtn.removeAttribute('disabled');
-    } else if (i > 0 && i === 3) {        
+
+    if (i >= 3) {
         prevBtn.removeAttribute('disabled');
         nextBtn.setAttribute('disabled', "disabled");
+    } else if (i > 0 && i < 3) {
+        prevBtn.removeAttribute('disabled');
+        nextBtn.removeAttribute('disabled');
+    } else {
+        prevBtn.setAttribute('disabled', "disabled");
+        nextBtn.removeAttribute('disabled');
     };
+
 };
-
-
